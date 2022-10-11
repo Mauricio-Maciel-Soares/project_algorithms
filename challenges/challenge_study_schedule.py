0b2f316e7ae41b6d7bf8bdb14da0e31282fab75d
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-11 18:28:38 -0300

    refac: reducing complexity req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,20 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    array_range = []
     count = 0
-
-    for index in permanence_period:
-        array_range.append(list(index))
-
-    for index in array_range:
-        for i in index:
-            if type(i) != int:
-                return None
 
     if target_time is None:
         return None
 
-    for num in array_range:
-        if num[0] <= target_time <= num[1]:
+    for index, i in permanence_period:
+        if type(index) != int or type(i) != int:
+            return None
+        if index <= target_time <= i:
             count += 1
 
     return count
