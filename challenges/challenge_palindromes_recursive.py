4b208079c9d99f1437314d0155b3bac7c0302a89
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-11 17:38:39 -0300

    chore: req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if word == "":
+        return False
+
+    if word[low_index] == word[high_index] and low_index <= high_index:
+        if len(word) == 1:
+            return True
+        if low_index + 1 == high_index - 1 or low_index + 1 == high_index:
+            return True
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
