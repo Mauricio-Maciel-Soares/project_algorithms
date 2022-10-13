04c7612f31187ed33136228b605f06172bd673d5
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-13 12:22:35 -0300

    fix: req 03, but is too complex(10), quick sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,24 +1,43 @@
 def is_anagram(first_string, second_string):
-    if first_string == "" or second_string == "":
-        return False
 
     f_s = list(first_string.lower())
     s_s = list(second_string.lower())
 
-    str_list = [f_s, s_s]
+    def ordena(lts):
+        length_lst = len(lts)
+        if length_lst > 0:
+            quick_sort(lts, 0, length_lst - 1)
+        else:
+            return False
 
-    for index in str_list:
-        n = len(index)
-        for i in range(n):
-            for j in range(n - i - 1):
-                if index[j] > index[j + 1]:
-                    index[j], index[j + 1] = index[j + 1], index[j]
+    def quick_sort(lts, start, end):
+        if start > end:
+            return
+        previous = start
+        posterior = end
+        pivot = lts[start]
 
-    # equality = True
-    # for i in range(len(f_s)):
-    #     if f_s[i] == s_s[i] and equality:
-    #         i += 1
-    #     else:
-    #         equality = False
+        while previous < posterior:
+            while previous < posterior and lts[posterior] > pivot:
+                posterior = posterior - 1
+
+            if previous < posterior:
+                lts[previous] = lts[posterior]
+                previous = previous + 1
+
+            while previous < posterior and lts[previous] <= pivot:
+                previous = previous + 1
+
+            if previous < posterior:
+                lts[posterior] = lts[previous]
+                posterior = posterior - 1
+
+            lts[previous] = pivot
+
+        quick_sort(lts, start, previous - 1)
+        quick_sort(lts, previous + 1, end)
+
+    ordena(f_s)
+    ordena(s_s)
 
     return f_s == s_s
