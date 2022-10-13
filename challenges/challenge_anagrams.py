19cd81680a7c76ec12f0ee86ff45bca289527eaf
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-13 10:01:14 -0300

    chore: req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if first_string == "" or second_string == "":
+        return False
+
+    f_s = list(first_string.lower())
+    s_s = list(second_string.lower())
+
+    str_list = [f_s, s_s]
+
+    for index in str_list:
+        n = len(index)
+        for i in range(n):
+            for j in range(n - i - 1):
+                if index[j] > index[j + 1]:
+                    index[j], index[j + 1] = index[j + 1], index[j]
+
+    iguais = True
+    for i in range(len(f_s)):
+        if f_s[i] == s_s[i] and iguais:
+            i += 1
+        else:
+            iguais = False
+
+    return iguais
