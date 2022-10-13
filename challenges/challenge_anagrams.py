4ec6204c4c7d4b1adf0ee6898914d4c9f7cee6f6
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-13 11:31:13 -0300

    fix: req 03, but is too complex(8)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,37 +7,12 @@ def is_anagram(first_string, second_string):
 
     str_list = [f_s, s_s]
 
-    def merge_sort(string, start=0, end=None):
-        if end is None:
-            end = len(string)
-        if (end - start) > 1:
-            mid = (start + end) // 2
-            merge_sort(string, start, mid)
-            merge_sort(string, mid, end)
-            merge(string, start, mid, end)
-
-    def merge(string, start, mid, end):
-        left = string[start:mid]
-        right = string[mid:end]
-
-        left_index, right_index = 0, 0
-
-        for general_index in range(start, end):
-            if left_index >= len(left):
-                string[general_index] = right[right_index]
-                right_index = right_index + 1
-            elif right_index >= len(right):
-                string[general_index] = left[left_index]
-                left_index = left_index + 1
-            elif left[left_index] < right[right_index]:
-                string[general_index] = left[left_index]
-                left_index = left_index + 1
-            else:
-                string[general_index] = right[right_index]
-                right_index = right_index + 1
-
     for index in str_list:
-        merge_sort(index, 0, len(index))
+        n = len(index)
+        for i in range(n):
+            for j in range(n - i - 1):
+                if index[j] > index[j + 1]:
+                    index[j], index[j + 1] = index[j + 1], index[j]
 
     equality = True
     for i in range(len(f_s)):
