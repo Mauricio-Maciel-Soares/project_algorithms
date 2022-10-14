b582dddeed60c5307cda91de0db3d78ce54af75e
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-10-14 18:50:09 -0300

    fix: req 03 done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,25 +4,28 @@ def is_anagram(first_string, second_string):
 
     f_s = list(first_string.lower())
     s_s = list(second_string.lower())
+    len_fs = len(f_s) - 1
+    len_ss = len(s_s) - 1
 
-    def quick_sort(lts, start, end):
-        if start < end:
-            previous = partition(lts, start, end)
-            quick_sort(lts, start, previous - 1)
-            quick_sort(lts, previous + 1, end)
-
-    def partition(lst, start, end):
-        pivot = lst[end]
-        delimiter = start - 1
-        for index in range(start, end):
-            if lst[index] <= pivot:
-                delimiter += 1
-                lst[delimiter], lst[index] = lst[index], lst[delimiter]
-        lst[delimiter + 1], lst[end] = lst[end], lst[delimiter + 1]
-
-        return delimiter + 1
-
-    quick_sort(f_s, 0, len(f_s) - 1)
-    quick_sort(s_s, 0, len(s_s) - 1)
+    quick_sort(f_s, 0, len_fs)
+    quick_sort(s_s, 0, len_ss)
 
     return f_s == s_s
+
+def quick_sort(lts, start, end):
+    if start < end:
+        previous = partition(lts, start, end)
+        quick_sort(lts, start, previous - 1)
+        quick_sort(lts, previous + 1, end)
+
+
+def partition(lst, start, end):
+    pivot = lst[end]
+    delimiter = start - 1
+    for index in range(start, end):
+        if lst[index] <= pivot:
+            delimiter += 1
+            lst[delimiter], lst[index] = lst[index], lst[delimiter]
+    lst[delimiter + 1], lst[end] = lst[end], lst[delimiter + 1]
+
+    return delimiter + 1
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) == 1 or nums == []:
+        return False
+
+    num_list = []
+    count_list = []
+
+    for index in nums:
+        if index is str:
+            return False
+        elif index not in num_list:
+            num_list.append(index)
+        elif index not in count_list:
+            count_list.append(index)
+
+    if len(count_list) == 1 and count_list[0] >= 0:
+        return count_list[0]
+    else:
+        return False
